f8d126b93374b0ef3ce6c9449c01fbcfe7735b69
YuraShabunyaWork <yura.shabunya.work@gmail.com>
2024-11-30 21:47:06 +0300

    Add project files.

---
+++ Chat/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+Console.WriteLine("Enter name: ");
+string? userName = Console.ReadLine();
+Console.WriteLine("Enter receive port: ");
+if (!int.TryParse(Console.ReadLine(), out var localPort)) return;
+Console.WriteLine("Enter send port");
+if(!int.TryParse(Console.ReadLine(), out var remotePort)) return;
+Console.WriteLine();
+
+Task.Run(ReceiveMessageAsync);
+await SendMessageAsync();
+
+async Task SendMessageAsync()
+{
+    using UdpClient sender = new UdpClient();
+    Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+
+    while(true)
+    {
+        var message = Console.ReadLine();
+        if(string.IsNullOrEmpty(message)) break;
+        message = $"{userName}: {message}";
+        var data = Encoding.UTF8.GetBytes(message);
+        await sender.SendAsync(data, new IPEndPoint(iPAddress, remotePort));
+    }
+}
+
+async Task ReceiveMessageAsync()
+{
+    using UdpClient receiver = new UdpClient(localPort);
+    while (true)
+    {
+        var result = await receiver.ReceiveAsync();
+        var message = Encoding.UTF8.GetString(result.Buffer);
+        Console.WriteLine(message);
+    }
+}
+
---
+++ ClientUdp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var udpClient = new UdpClient();
+udpClient.Connect(IPAddress.Parse("127.0.0.1"), 5555);
+while (true)
+{
+    string? message = Console.ReadLine();
+    byte[] data = Encoding.UTF8.GetBytes(message);
+    //IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+    int bytes = await udpClient.SendAsync(data);
+    Console.WriteLine($"Оправленно {bytes} байт");
+}
